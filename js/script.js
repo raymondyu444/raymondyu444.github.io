24d67aa36771ca630dd3918f3136184d8cfee3f2
Raymond Yu <Ray@Raymonds-MacBook-Pro.local>
2020-01-19 23:27:06 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$("#services").text("Our Services");
+
+	$('.disclaimer').click(function() {
+	  alert('Please read the disclaimer!!!');
+	});
+});
+
+$(window).resize(function () {
+    var viewportWidth = $(window).width();
+    if (viewportWidth < 576) {
+            $(".show").removeClass("show");
+    } else {
+    	$("#collapseOne").addClass("show");
+    };
+});
